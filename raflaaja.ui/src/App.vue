a770fadbebfc3c4efddaf175950ce60b9ef40d11
Jetro <jetro.karjalainen@gmail.com>
2022-10-08 15:40:45 +0300

    added font

--- raflaaja.ui/src/App.vue
+++ raflaaja.ui/src/App.vue
@@ -17,8 +17,8 @@ export default {
 </script>
 
 <style>
-
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;1,100;1,300&display=swap');
 #app {
-
+    font-family: 'Roboto', sans-serif;
 }
 </style>
